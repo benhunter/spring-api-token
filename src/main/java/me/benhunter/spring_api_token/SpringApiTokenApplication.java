f6a13d1fc9d0cbab5f9f034f31a91f01e67a5d73
Ben Hunter <code@benhunter.me>
2024-08-25 19:16:19 -0500

    Add Spring Starter

---
+++ src/main/java/me/benhunter/spring_api_token/SpringApiTokenApplication.java
@@ -0,0 +1,13 @@
+package me.benhunter.spring_api_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/benhunter/spring_api_token/SpringApiTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package me.benhunter.spring_api_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
